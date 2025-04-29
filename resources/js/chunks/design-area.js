37691eb27613ab7d967ea77cba4b92bac9b2cc0d
Giorgio Tempesta <32170885+gtempesta-pixartprinting@users.noreply.github.com>
2025-04-29 10:01:01 +0200

    updated syntax

--- resources/js/chunks/design-area.js
+++ resources/js/chunks/design-area.js
@@ -1,10 +1,20 @@
-import designArea from 'remote_bucket/mountMyDesignArea';
+async function runProcess() {
+  const { default: remoteMountMethod } = await import(
+    'remote/mountMyDesignArea'
+  );
+  // const { default: remoteMountMethod } = await import(
+  //   'remote_bucket/mountMyDesignArea'
+  // );
 
-const { default: remoteMountMethod } = designArea;
-const rootElement = document.getElementById('react-root');
-const accessToken = window.anonToken;
-const customerId = '12345';
-remoteMountMethod(rootElement, {
-  customerId,
-  token: accessToken,
-});
+  console.log('test check check');
+
+  const rootElement = document.getElementById('react-root');
+  const accessToken = window.anonToken;
+  const customerId = '12345';
+  remoteMountMethod(rootElement, {
+    customerId,
+    token: accessToken,
+  });
+}
+
+runProcess();
