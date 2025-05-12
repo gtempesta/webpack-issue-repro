e0ee6db94a3fe911702b0205c928aed45c5a6dcd
Giorgio Tempesta <32170885+gtempesta-pixartprinting@users.noreply.github.com>
2025-05-12 10:49:05 +0200

    code change

--- resources/js/chunks/design-area.js
+++ resources/js/chunks/design-area.js
@@ -1,10 +1,5 @@
 async function runProcess() {
-  const { default: designAreaModule } = await import(
-    'remote/mountMyDesignArea'
-  );
-  // const { default: designAreaModule } = await import('remote_bucket/mountMyDesignArea');
-  // extract mount method and eventBus from the remote module
-  const { default: remoteMountMethod } = designAreaModule;
+  const { default: remoteMountMethod } = window.designArea;
 
   const rootElement = document.getElementById('react-root');
   const accessToken = window.anonToken;
@@ -13,8 +8,7 @@ async function runProcess() {
     customerId,
     token: accessToken,
   });
-  console.log("Design area module updated at", new Date().toISOString());
-
+  console.log('Design area module updated at', new Date().toISOString());
 }
 
 runProcess();
